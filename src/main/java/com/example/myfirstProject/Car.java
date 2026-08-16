3bdd5ff9bcf7e7f4264e3bb02c631609063d650d
abdullah58091 <abdullahjd6@gmail.com>
2026-08-16 14:56:58 +0530

    Day-01

---
+++ src/main/java/com/example/myfirstProject/Car.java
@@ -0,0 +1,11 @@
+package com.example.myfirstProject;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Car {
+
+    public void drive() {
+        System.out.println("Car is running...");
+    }
+}
